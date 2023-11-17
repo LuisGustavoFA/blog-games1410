6f41b846091c44a69677107c7642d18d167e305c
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2023-11-17 17:29:59 -0300

    updating UI/UX v1.08

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
     <Router>
       <Header/>
         <Routes>
-          <Route path='' element={<HomePage/>}/>
-          <Route path='/home' element={<HomePage/>}/>
-          <Route path='/article/:title' element={<Article/>}/>
-          <Route path='/list/:title' element={<ListPage/>}/>
-          <Route path='/search/:search' element={<SearchPage/>}/>
-          <Route path='*' element={<ErrorPage/>}/>
+          <Route element={<HomePage/>} path=''/>
+          <Route element={<HomePage/>} path='/home'/>
+          <Route element={<Article/>} path='/article/:title'/>
+          <Route element={<ListPage/>} path='/list/:title'/>
+          <Route element={<SearchPage/>} path='/search/:search'/>
+          <Route element={<ErrorPage/>} path='*'/>
         </Routes>
       <Footer/>
     </Router>
