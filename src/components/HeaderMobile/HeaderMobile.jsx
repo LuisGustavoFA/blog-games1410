f536a744d19981707a836578d952377e8b20eeaf
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2023-11-20 16:26:22 -0300

    updating UI/UX

--- src/components/HeaderMobile/HeaderMobile.jsx
+++ src/components/HeaderMobile/HeaderMobile.jsx
@@ -2,39 +2,54 @@ import styles from './HeaderMobile.module.css';
 import QuickMenu from '../QuickMenu/QuickMenu';
 import { BsSearch } from 'react-icons/bs';
 import { RxCross1 } from "react-icons/rx";
+import { MdMenu } from "react-icons/md";
 import { useState, useEffect } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import SearchBarNew from '../SearchBarNew/SearchBarNew';
 
 function HeaderMobile() {
-  const [active, setActive] = useState(false);
+  const [currentSubheader, setCurrentSubheader] = useState(null);
   const location = useLocation();
 
-  const handlerButton = () => {
-    setActive(!active);
+  const handleSubheader = (subheader) => {
+    if (subheader === currentSubheader) {
+      setCurrentSubheader(null);
+    } else {
+      if (currentSubheader === null) {
+        setCurrentSubheader(subheader);
+      } else {
+        setCurrentSubheader(null);
+        setTimeout(()=> setCurrentSubheader(subheader), 200);
+      }
+    }
   }
 
   useEffect(() => {
-    if (active) {
-      setActive(false);
-    }
+    setCurrentSubheader(null);
   }, [location]);
 
   return (
     <header className={styles.header}>
       <ul className={styles.header_mobile}>
-        <li><QuickMenu /></li>
+        <li>
+          {currentSubheader === 'quickMenu' ?
+            <RxCross1 className={styles.header_mobile_buttons} size={32} onClick={() => handleSubheader('quickMenu')}/> :
+            <MdMenu className={styles.header_mobile_buttons} size={32} onClick={() => handleSubheader('quickMenu')}/>
+          }
+        </li>
         <li><Link className={styles.header_mobile_logo} to={"/home"}>GAMES BLOG</Link></li>
         <li>
-          {active ? 
-            <RxCross1 className={styles.header_mobile_search_button} size={32} onClick={handlerButton}/> :
-            <BsSearch className={styles.header_mobile_search_button} size={32} onClick={handlerButton}/>
+          {currentSubheader === 'search' ?
+            <RxCross1 className={styles.header_mobile_buttons} size={32} onClick={() => handleSubheader('search')}/> :
+            <BsSearch className={styles.header_mobile_buttons} size={32} onClick={() => handleSubheader('search')}/>
           }
-          
         </li>
       </ul>
-      <div className={`${styles.header_mobile_search_div} ${active ? styles.active : ''}`}>
-        {active ? <SearchBarNew mobile/> : ""}
+      <div className={`${styles.header_mobile_subheader} ${currentSubheader === 'quickMenu' ? styles.active : ''}`}>
+        {currentSubheader === 'quickMenu' ? <QuickMenu/> : ""}
+      </div>
+      <div className={`${styles.header_mobile_subheader} ${currentSubheader === 'search' ? styles.active : ''}`}>
+        {currentSubheader === 'search' ? <SearchBarNew mobile/> : ""}
       </div>
     </header>
   )
--- src/components/QuickMenu/QuickMenu.jsx
+++ src/components/QuickMenu/QuickMenu.jsx
@@ -1,34 +1,15 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from "./QuickMenu.module.css";
-import { useState, useEffect } from 'react';
 import DarkToggle from '../DarkToggle/DarkToggle';
 
 function QuickMenu() {
-  const [active, setActive] = useState(false);
-  const location = useLocation();
-
-  const handlerButton = () => {
-    setActive(!active);
-  }
-
-  useEffect(() => {
-    if (active) {
-      setActive(false);
-    }
-  }, [location]);
-
   return (
-    <>
-      <div className={`${styles.sandwichButton} ${active ? styles.active : ''}`} onClick={handlerButton}></div>  
-      <div className={`${styles.sandwich} ${active ? styles.active : ''}`}>
-        <ul className={`${styles.sandwichList} ${active ? styles.active : ''}`}>
-          <Link className={styles.sandwichLinks} to={"/search/noticia"}>Notícias</Link>
-          <Link className={styles.sandwichLinks} to={"/search/review"}>Reviews</Link>
-          <Link className={styles.sandwichLinks} to={"/lists"}>Listas</Link>
-          <DarkToggle mobile/>
-        </ul>
-      </div>
-    </>
+    <ul className={styles.quickMenuCase}>
+      <Link className={styles.quickMenuLinks} to={"/search/noticia"}>Notícias</Link>
+      <Link className={styles.quickMenuLinks} to={"/search/review"}>Reviews</Link>
+      <Link className={styles.quickMenuLinks} to={"/lists"}>Listas</Link>
+      <DarkToggle mobile/>
+    </ul>
   )
 }
 
