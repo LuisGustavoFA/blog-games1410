3b2aac25e10238a79a207b5eab1983562c0e5d2a
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2023-11-24 10:45:03 -0300

    updating UI/UX

--- src/components/ReviewSection/ReviewSection.js
+++ src/components/ReviewSection/ReviewSection.js
@@ -2,11 +2,20 @@ import React, { useEffect, useRef, useState } from 'react'
 import "./ReviewSection.css";
 import CardReview from "../CardReview/CardReview";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { getData } from "../../database/DataApi";
 
 function ReviewSection() {
   const [leftArrowActive, setLeftArrowActive] = useState(false);
   const [rightArrowActive, setRightArrowActive] = useState(true);
   const reviewsCardsRef = useRef(null);
+
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    getData().then((resp) => {
+      setNoticias(resp);
+    })
+  }, []);
 
   const scroll = (dir) => {
     const reviewsCards = reviewsCardsRef.current;
@@ -18,13 +27,14 @@ function ReviewSection() {
     const reviewsCards = reviewsCardsRef.current;
     const currentScroll = reviewsCards.scrollLeft;
     const maxScroll = reviewsCards.scrollWidth - reviewsCards.clientWidth;
-
+    
     setLeftArrowActive(currentScroll >= 1);
     setRightArrowActive(currentScroll < maxScroll -1);
   }
 
   useEffect(()=> {
     const reviewsCards = reviewsCardsRef.current;
+    handleActivation();
     reviewsCards.addEventListener("scroll", handleActivation);
     return () => {
       reviewsCards.removeEventListener("scroll", handleActivation);
@@ -39,7 +49,12 @@ function ReviewSection() {
           <FaArrowLeft className='reviews-arrows' size={38}/>
         </span>
 
-        <CardReview title={'Resident Evil 4'} score={'95'} image={'https://cdn.promo.capcomusa.com/boxart/157136.png'}/>
+        {noticias.map((noticia) => {
+          const isReview = noticia.tags.includes("REVIEW");
+          return (isReview && <CardReview title={noticia.title} score={'86'} image={noticia.banner}/>);
+        })}
+
+        {/* <CardReview title={'Resident Evil 4'} score={'95'} image={'https://cdn.promo.capcomusa.com/boxart/157136.png'}/>
         <CardReview title={'Resident Evil 7'} score={'90'} image={'https://cdn.promo.capcomusa.com/boxart/16073.png'}/>
         <CardReview title={'Devil May Cry 5'} score={'90'} image={'https://cdn.promo.capcomusa.com/boxart/11942.png'}/>
         <CardReview title={'Mafia: Definitive Edition'} score={'80'} image={'https://cdn.2kgames.com/web/mafiagame.com/definitive-edition/buy-03-game-m1.jpg'}/>
@@ -48,7 +63,7 @@ function ReviewSection() {
         <CardReview title={'Resident Evil 7'} score={'90'} image={'https://cdn.promo.capcomusa.com/boxart/16073.png'}/>
         <CardReview title={'Devil May Cry 5'} score={'90'} image={'https://cdn.promo.capcomusa.com/boxart/11942.png'}/>
         <CardReview title={'Mafia: Definitive Edition'} score={'80'} image={'https://cdn.2kgames.com/web/mafiagame.com/definitive-edition/buy-03-game-m1.jpg'}/>
-        <CardReview title={'KINGDOM HEARTS HD 1.5 + 2.5 ReMIX'} score={'75'} image={'https://www.kingdomhearts.com/img/packaging/hd1525/kingdom-hearts-hd-15-25-packshot.jpg'}/>
+        <CardReview title={'KINGDOM HEARTS HD 1.5 + 2.5 ReMIX'} score={'75'} image={'https://www.kingdomhearts.com/img/packaging/hd1525/kingdom-hearts-hd-15-25-packshot.jpg'}/> */}
 
         <span className={`reviews-arrows-case-right ${rightArrowActive ? " active" : ""}`} onClick={() => scroll("right")}>
           <FaArrowRight className='reviews-arrows' size={38}/>
