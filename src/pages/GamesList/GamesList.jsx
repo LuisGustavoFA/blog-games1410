c029ede4ca1d77511c44d725d0e8bb34a35d62e9
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2023-12-10 21:01:34 -0300

    updating UI/UX

--- src/pages/GamesList/GamesList.jsx
+++ src/pages/GamesList/GamesList.jsx
@@ -1,4 +1,7 @@
 import styles from './GamesList.module.css';
+import parse from 'html-react-parser';
+import YouTube from 'react-youtube';
+import React from 'react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { findArticle } from '../../database/DataApi';
@@ -15,17 +18,81 @@ function GamesList() {
   const { title } = useParams();
   const [article, setArticle] = useState([]);
   const [games, setGames] = useState([]);
-  const [tweet, setTweet] = useState('');
+  const [articleText, setArticleText] = useState('');
+  const [postIncluded, setPostIncluded] = useState(false);
+  const [videoIncluded, setVideoIncluded] = useState(false);
 
   document.title = article.title;
 
   useEffect(() => {
     findArticle(title.replace(/-{2,}/g, ' ').replace(/-/g, " ")).then((resp) => {
-      setArticle(resp);
       resp.games ? setGames(resp.games) : setGames([]);
-      resp.social?.x && setTweet(resp.social.x);
+      setArticle(resp);
     })
   }, [title])
+
+  useEffect(() => {
+    const post = article.social?.post;
+    const video = article.social?.video;
+
+    if (post && article.text.includes('[socialPost]')) {
+      setPostIncluded(true);
+    }
+
+    if (video && article.text.includes('[socialVideo]')) {
+      setVideoIncluded(true);
+    }
+
+    setArticleText(article.text);
+    console.log(articleText);
+
+  }, [article]);
+
+  const renderContent = () => {
+    if (!postIncluded && !videoIncluded) {
+      return articleText;
+
+    } else if (postIncluded && videoIncluded) {
+      let test = articleText.split('[socialPost]').map((part, index, array) => (
+        <React.Fragment key={index}>
+          {part}
+          {index !== array.length - 1 && <Tweet id={article.social?.post} />}
+        </React.Fragment>
+      ));
+
+      let test2 = test[1].props.children[0].split('[socialVideo]').map((part, index, array) => (
+        <React.Fragment key={index}>
+          {part}
+          {index !== array.length - 1 && <YouTube style={{paddingTop: "16px"}} videoId={article.social?.video} />}
+        </React.Fragment>
+      ));
+
+      let test3 = test.splice(0,1).concat(test2);
+
+      return test3;
+
+    } else if (postIncluded) {
+      let test = articleText.split('[socialPost]').map((part, index, array) => (
+        <React.Fragment key={index}>
+          {part}
+          {index !== array.length - 1 && <Tweet id={article.social?.post} />}
+        </React.Fragment>
+      ));
+
+      return test;
+
+    } else if (videoIncluded) {
+      let test = articleText.split('[socialVideo]').map((part, index, array) => (
+        <React.Fragment key={index}>
+          {part}
+          {index !== array.length - 1 && <YouTube style={{paddingTop: "16px"}} videoId={article.social?.video} />}
+        </React.Fragment>
+      ));
+
+      return test;
+    }
+    
+  };
 
   return (
     <main className={styles.main}>
@@ -38,11 +105,12 @@ function GamesList() {
         <span className={styles.banner_case_subtitle}>{article.subtitle}</span>
       </div>
       <div className={styles.content}>
-        <span className={styles.content_text} dangerouslySetInnerHTML={{ __html: article.text }}></span>
+        <span className={styles.content_text}>
+          {renderContent()}
+        </span>
         {games.map((game, index) => (
           <CardList key={index} game={game} />
         ))}
-        {tweet != '' && <Tweet id={tweet} />}
         <h5 className={styles.content_subtext} >Por <Link className={styles.content_link} to={`/search/${format(article.info?.autor)}`}>{article.info?.autor}</Link>, {calctime(article.info?.time)}.</h5>
       </div>
       <MoreArticles />
