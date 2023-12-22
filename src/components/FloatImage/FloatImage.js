7e0324b9f80a173b128dec63b150fa9733f38d2a
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2023-12-22 00:57:03 -0300

    updating UI/UX

--- src/components/FloatImage/FloatImage.js
+++ src/components/FloatImage/FloatImage.js
@@ -1,13 +1,15 @@
+import { useEffect } from "react";
 import "./FloatImage.css";
 import { RxCross1 } from "react-icons/rx";
 
 function FloatImage({ image, imgOpen, setImgOpen }) {
-  
-  if (imgOpen) {
-    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
-  } else {
-    document.getElementsByTagName('body')[0].style.overflow = 'visible';
-  }
+  useEffect(()=>{
+    if (imgOpen) document.body.style.overflow = 'hidden';
+    else document.body.style.overflow = 'visible';
+    return () => {
+      document.body.style.overflow = 'visible';
+    }
+  }, [imgOpen]);
 
   return (
     <>
--- src/pages/Tag/Tag.jsx
+++ src/pages/Tag/Tag.jsx
@@ -1,19 +1,24 @@
 import styles from './Tag.module.css';
 import { IoGameController } from "react-icons/io5";
 import { FaPlaystation } from "react-icons/fa";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import CardHorizontal from '../../components/CardHorizontal/CardHorizontal';
-import { findByTag } from '../../database/DataApi';
+import { findByTag, getData } from '../../database/DataApi';
 import CardBanner from '../../components/CardBanner/CardBanner';
 import AdCase from '../../components/AdCase/AdCase';
+import { format } from '../../functions/format';
+import { MdOutlineExpandMore } from "react-icons/md";
+import isMobileHandler from '../../components/IsMobileHandler/IsMobileHandler';
 
 function Tag() {
   const { tag } = useParams();
   const [noticias, setNoticias] = useState([]);
   const [banner, setBanner] = useState([]);
   const [open, setOpen] = useState(false);
-  const [opacity, setOpacity] = useState("0%");
+  const [tags, setTags] = useState([]);
+  const [data, setData] = useState({});
+  const isMobile = isMobileHandler();
 
   useEffect(() => {
     findByTag(tag).then((resp) => {
@@ -23,16 +28,45 @@ function Tag() {
     window.scrollTo(0, 0);
   }, [tag]);
 
+  const extractTags = (data) => {
+      const allTags = data.reduce((tags, item) => {
+        tags.push(...item.tags);
+        if (item.games) {
+          item.games.forEach(game => {
+            if (game.tags) {
+              tags.push(...game.tags);
+            }
+          });
+        }
+        return tags;
+      }, []);
+  
+    const uniqueTags = [...new Set(allTags)];
+    return uniqueTags;
+  }
+
+  useEffect(()=>{
+    getData().then((resp)=>{
+      const extTags = extractTags(resp);
+      setTags(extTags);
+      setData(resp);
+    });
+  }, [tag]);
+
+  const countTag = (tag) => {
+    let count = 0;
+    for (const item of data) {
+      if (item.tags.includes(tag)) count +=1;
+    }
+    return count;
+  }
+
   const openViewMore = () => {
     setOpen(true);
-    setTimeout(()=> {
-      setOpacity("100%");
-    }, 100);
   }
 
   const closeViewMore = () => {
     setOpen(false);
-    setOpacity("0%");
   }
 
   const handleViewMore = () => {
@@ -40,35 +74,29 @@ function Tag() {
     else openViewMore();
   }
 
-  useEffect(()=> {
-    const handleScroll = () => closeViewMore();
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }, []);
-
   return (
     <main>
       <div className={styles.banner}>
         <div className={styles.banner_content}>
-          <div className={styles.banner_title}>
-            {tag == 'sony' ? <FaPlaystation className={styles.banner_title_icon} /> : <IoGameController className={styles.banner_title_icon} />}
-            <span className={styles.banner_title_span}>{tag.toUpperCase()}</span>
-          </div>
-          <span className={styles.banner_viewMore} onMouseEnter={()=> openViewMore()} onMouseLeave={()=> closeViewMore()} onClick={()=> handleViewMore()}>
-            MAIS TAGS
-            <div className={styles.banner_viewMore_content} style={{height: `${open ? "400px" : "0px"}`, border: `${open ? "1px solid #FFF" : "1px solid var(--main-color)"}`}}>
-              <div className={styles.banner_viewMore_content_tags} style={{display: `${open ? "flex" : "none"}`, opacity: `${opacity}`}}>
-                <h2 className={styles.banner_viewMore_content_tag}>TESTE</h2>
-                <h2 className={styles.banner_viewMore_content_tag}>PLAYSTATION</h2>
-                <h2 className={styles.banner_viewMore_content_tag}>PC</h2>
-                <h2 className={styles.banner_viewMore_content_tag}>MOBILE</h2>
-                <h2 className={styles.banner_viewMore_content_tag}>ROCKSTAR</h2>
-                <h2 className={styles.banner_viewMore_content_tag}>SONY</h2>
-              </div>
+          <div className={styles.banner_content_leftHand}>
+            <div className={styles.banner_title}>
+              {tag == 'sony' ? <FaPlaystation className={styles.banner_title_icon} /> : <IoGameController className={styles.banner_title_icon} />}
+              <span className={styles.banner_title_span}>{tag.toUpperCase()}</span>
             </div>
-          </span>
+            <span className={styles.banner_viewMore} onMouseLeave={()=> closeViewMore()} onClick={()=> handleViewMore()}>
+              <MdOutlineExpandMore className={styles.banner_viewMore_icon} size={isMobile ? 28 : 32} onMouseEnter={()=> openViewMore()}/>
+              <div className={styles.banner_viewMore_content} style={{height: `${open ? "auto" : "0px"}`}}>
+                <div className={styles.banner_viewMore_content_tags} style={{display: `${open ? "flex" : "none"}`}}>
+                  {tags.sort().map((tag)=>
+                    <Link className={styles.banner_viewMore_content_tag} to={`/tag/${format(tag)}`}>
+                      {tag}
+                      <span className={styles.banner_viewMore_content_tagCount}>{countTag(tag)}</span>
+                    </Link>
+                  )}
+                </div>
+              </div>
+            </span>
+          </div>
         </div>
       </div>
       <div className={styles.news}>
