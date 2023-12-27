f53e9ecb27e081339fac70dd2717879de095cd41
João Gabriel FA <joaogabrielfa.dev@gmail.com>
2023-12-26 23:31:59 -0300

    updating UI/UX

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import ReviewSection from "../../components/ReviewSection/ReviewSection";
 import CardVertical from "../../components/CardVertical/CardVertical";
 import BannerSliderNew from "../../components/BannerSliderNew/BannerSliderNew";
 import AdCase from '../../components/AdCase/AdCase';
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 function HomePage() {
   window.scrollTo(0, 0);
@@ -13,11 +14,14 @@ function HomePage() {
   
   const [noticias, setNoticias] = useState([]);
   const [lists, setLists] = useState([]);
+  const [page, setPage] = useState(1);
+  const [maxPages, setMaxPages] = useState(0);
 
   useEffect(() => {
     getData('news').then((resp) => {
       setNoticias(resp);
     })
+    setMaxPages(Math.ceil(noticias.length / 10));
   }, []);
 
   useEffect(() => {
@@ -33,17 +37,21 @@ function HomePage() {
       <section className="home-content">
         <section className="home-content-main">
           <h2 className="home-content-title">ÚLTIMAS NOTÍCIAS</h2>
-          {noticias.map((noticia, id) => {
-            const isReview = noticia.tags.includes("REVIEW");
-            if (!isReview) {
+          {noticias.slice(((page - 1) * 10), (page * 10)).map((noticia, id) => {
               return (
                 <React.Fragment key={id}>
                   <CardHorizontal data={noticia} key={id}/>
                   {((id - 1) % 3 === 0) && <AdCase/>}
                 </React.Fragment>
               )
-            }
           })}
+          
+          <div className="home-content-main-pageButtons">
+            <FaArrowLeft onClick={()=> page > 1 ? setPage(page - 1) : ""}/>
+            <div>{`${page}`}</div>             
+            <FaArrowRight onClick={()=> page < maxPages ? setPage(page + 1) : ""}/>
+          </div>
+
         </section>
 
         <section className="home-content-secondary">
